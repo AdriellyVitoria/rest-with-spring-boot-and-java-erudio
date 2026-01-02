b339660206d3d4b65f7043db55ec43e39b6d1a51
Adrielly <mendes.silva.vitoria.adrielly@gmail.com>
2026-01-02 14:16:03 -0300

    feat: ajuste no long

--- rest-with-spring-boot-and-java-erudio/src/main/java/br/com/AdriellyVitoria/erudio/services/PersonServices.java
+++ rest-with-spring-boot-and-java-erudio/src/main/java/br/com/AdriellyVitoria/erudio/services/PersonServices.java
@@ -3,18 +3,20 @@ package br.com.AdriellyVitoria.erudio.services;
 import br.com.AdriellyVitoria.erudio.exception.ResourceNotFoundException;
 import br.com.AdriellyVitoria.erudio.model.Person;
 import br.com.AdriellyVitoria.erudio.repository.PersonRespository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
-import java.util.logging.Logger;
+
 
 @Service
 public class PersonServices {
 
     private final AtomicLong counter = new AtomicLong();
-    private Logger logger = Logger.getLogger(PersonServices.class.getName());
+    private Logger logger = LoggerFactory.getLogger(PersonServices.class.getName());
 
     @Autowired // Para fazer a injenção de dependencia
     PersonRespository respository;

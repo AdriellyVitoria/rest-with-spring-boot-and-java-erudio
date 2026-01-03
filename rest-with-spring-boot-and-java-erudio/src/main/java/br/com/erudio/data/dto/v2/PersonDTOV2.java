d84fe1eefd4921afd5d96d190fa0c6debff3fd3c
Adrielly <mendes.silva.vitoria.adrielly@gmail.com>
2026-01-03 11:26:39 -0300

    feat: estudando versionamento de api

---
+++ rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/data/dto/v2/PersonDTOV2.java
@@ -0,0 +1,78 @@
+package br.com.erudio.data.dto.v2;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Objects;
+
+
+public class PersonDTOV2 implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private long id;
+    private String firstName;
+    private String lastName;
+    private Date birthDate;
+    private String address;
+    private String gender;
+
+    public PersonDTOV2() {}
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof PersonDTOV2 that)) return false;
+        return getId() == that.getId() && Objects.equals(getFirstName(), that.getFirstName()) && Objects.equals(getLastName(), that.getLastName()) && Objects.equals(getBirthDate(), that.getBirthDate()) && Objects.equals(getAddress(), that.getAddress()) && Objects.equals(getGender(), that.getGender());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getFirstName(), getLastName(), getBirthDate(), getAddress(), getGender());
+    }
+}

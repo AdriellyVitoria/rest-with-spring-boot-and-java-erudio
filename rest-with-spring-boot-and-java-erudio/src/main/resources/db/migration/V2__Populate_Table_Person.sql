7bf4d7ca44a15b4a47dfc9bb7a2cb492fbae6e41
Adrielly <mendes.silva.vitoria.adrielly@gmail.com>
2026-01-05 23:22:31 -0300

    feat: criacao de migration

---
+++ rest-with-spring-boot-and-java-erudio/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,8 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+        (1, 'mary', 'Fimendes', 'Male', 'netoo'),
+        (4, 'Brail', 'mateus', 'mendes', 'nunes'),
+        (6, 'PAris', 'Milena', 'Male', 'Leite0'),
+        (8, 'aryeli', 'Fimendes', 'Male', 'netoo'),
+        (9, 'aryeli', 'Fimendes', 'Male', 'netoo'),
+        (10, 'aryeli', 'Fimendes', 'Male', 'netoo');
+

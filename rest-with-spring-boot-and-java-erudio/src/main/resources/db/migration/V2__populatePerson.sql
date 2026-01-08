f970f663115a36c6f6a5265b33acfab581bc5311
Adrielly <mendes.silva.vitoria.adrielly@gmail.com>
2026-01-07 22:51:57 -0300

    feat: ajuste nas migration usando postgresql

---
+++ rest-with-spring-boot-and-java-erudio/src/main/resources/db/migration/V2__populatePerson.sql
@@ -0,0 +1,8 @@
+
+INSERT INTO person (address, first_name, gender, last_name) VALUES
+            ('mary', 'Fimendes', 'Male', 'netoo'),
+            ('Brasil', 'Mateus', 'Male', 'Nunes'),
+            ('Paris', 'Milena', 'Female', 'Leite'),
+            ('Aryeli', 'Fimendes', 'Male', 'Netoo'),
+            ('Aryeli', 'Fimendes', 'Male', 'Netoo'),
+            ('Aryeli', 'Fimendes', 'Male', 'Netoo');
